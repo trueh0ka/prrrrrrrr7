30c720f95862835467c520c8a9a0efff17b0f354
trueh0ka <isip_a.s.nikolaev@mpt.ru>
2023-05-13 21:49:38 +0300

    asd

---
+++ vipief/Model/Colour.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vipief.Model
+{
+    public class Colour
+    {
+        public Colour(string name, int kolichstvo, float price)
+        {
+            Name = name;
+            Kolichstvo = kolichstvo;
+            Price = price;
+        }
+
+        public Colour() { }
+
+        public string Name { get; set; }
+        public int Kolichstvo { get; set; }
+        public float Price { get; set; }
+    }
+}
---
+++ vipief/ViewModel/MainViewModel.cs
@@ -0,0 +1,87 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using vipief.Model;
+using vipief.ViewModel.Helpers;
+
+namespace vipief.ViewModel
+{
+    internal class MainViewModel : BindingHelper
+    {
+        #region Команды
+        public BindableCommand AddCommand { get; set; }
+        public BindableCommand DeleteCommand { get; set; }
+        public BindableCommand SohranenieCommand { get; set; }
+        #endregion
+
+        #region Свойства
+
+        private Colour selected = new Colour();
+        public Colour Selected
+        {
+            get { return selected; }
+            set
+            {
+                selected = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<Colour> colours;
+        public ObservableCollection<Colour> Colours
+        {
+            get { return colours; }
+            set
+            {
+                colours = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        public void AddItems()
+        {
+            Colours.Add(Selected);
+        }
+
+        public void DeleteItems()
+        {
+            Colours.Remove(Selected);
+        }
+
+        public void Sohranenie()
+        {
+            ObservableCollection<Colour> cc = new ObservableCollection<Colour>();
+            cc = Colours;
+            string json = JsonConvert.SerializeObject(cc);
+            File.WriteAllText("C:\\Users\\Алексей\\OneDrive\\Рабочий стол\\Result.json", json);
+        }
+
+        public MainViewModel()
+        {
+            string text = File.ReadAllText("C:\\Users\\Алексей\\OneDrive\\Рабочий стол\\Result.json");
+            List<Colour> result = JsonConvert.DeserializeObject<List<Colour>>(text);
+            ObservableCollection<Colour> colours = new ObservableCollection<Colour>();
+            for (int i = 0; i <= result.Count-1; i++)
+            {
+                colours.Add(result[i]);
+            }
+            Colours = colours;
+            AddCommand = new BindableCommand(_ => AddItems());
+            DeleteCommand = new BindableCommand(_ => DeleteItems());
+            SohranenieCommand = new BindableCommand(_ => Sohranenie());
+        }
+    }
+}
